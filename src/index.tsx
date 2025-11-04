c9b13dcff6c1d1af8fb7627bbcefae4a2be9f40b
davlatjanov <ndavlatjonov@gmail.com>
2025-11-04 10:04:30 +0900

    feat: create routing system

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./app/MaterialTheme";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -17,7 +18,9 @@ root.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
