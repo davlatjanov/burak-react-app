e1d1fdf3424e0de3420b4326638862252fe96c98
davlatjanov <ndavlatjonov@gmail.com>
2025-12-04 21:43:59 +0900

    fix: store logger modified

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
-import { Home } from "@mui/icons-material";
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/homePage/slice";
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    // @ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
   reducer: {
     homePage: HomePageReducer,
   },
