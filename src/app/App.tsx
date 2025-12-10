0780e8c2992108a3d082dbe9938c0954d100f3d5
davlatjanov <ndavlatjonov@gmail.com>
2025-12-10 16:46:09 +0900

    feat: publish authentication model component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,11 +15,17 @@ import "../css/navbar.css";
 import "../css/footer.css";
 import { CartItem } from "../lib/types/search";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
 
 function App() {
   const location = useLocation();
 
   const { cartItems, onAdd, onDelete, onDeleteAll, onRemove } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+  /*HANDLERS*/
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -58,6 +64,12 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
+      />
     </>
   );
 }
